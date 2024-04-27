e80722eab84b56b57eda7f4481e6bba483bdf3dc
Avik Kundu <avikkundu8d@gmail.com>
2024-04-27 20:15:33 +0530

    up

--- devconsole/odc.sh
+++ devconsole/odc.sh
@@ -15,15 +15,15 @@ odc_stories+="\n1. <https://issues.redhat.com/browse/ODC-7566?filter=12435231 | 
 
 
 # Stories in Review
-odc_stories+="\n1. <https://issues.redhat.com/browse/ODC-7557?filter=12435230 | Stories In Review: > $(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435230' -H "Accept: application/json" | jq '.total')"
+odc_stories+="\n2. <https://issues.redhat.com/browse/ODC-7557?filter=12435230 | Stories In Review: > $(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435230' -H "Accept: application/json" | jq '.total')"
 
 
 # Unassigned Stories
-odc_stories+="\n1. <https://issues.redhat.com/issues/?filter=12435229 | Unassigned Stories: > ($(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435229' -H "Accept: application/json" | jq '.total')"
+odc_stories+="\n3. <https://issues.redhat.com/issues/?filter=12435229 | Unassigned Stories: > $(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435229' -H "Accept: application/json" | jq '.total')"
 
 
 # Ready for pointing
-odc_stories+="\n1. <https://issues.redhat.com/issues/?filter=12435232 | Ready to Point Stories: > $(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435232' -H "Accept: application/json" | jq '.total')"
+odc_stories+="\n4. <https://issues.redhat.com/issues/?filter=12435232 | Ready to Point Stories: > $(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435232' -H "Accept: application/json" | jq '.total')"
 
 echo "Fetching ODC bugs"
 
@@ -31,7 +31,7 @@ odc_bugs="\n*Bugs:*"
 
 # Bugs in Code Review
 bugs=$(curl -H "$authorization" -s 'https://issues.redhat.com/rest/api/2/search?jql=filter=12435233' -H "Accept: application/json" | jq '.total')
-odc_bugs+="\n1.  <https://issues.redhat.com/issues/?filter=12435233 | Bugs In Code Review: > $bugs"
+odc_bugs+="\n1. <https://issues.redhat.com/issues/?filter=12435233 | Bugs In Code Review: > $bugs"
 if [ $bugs -ge 20 ]; then
     odc_bugs+=" :fire::fire:"
 fi
@@ -71,7 +71,9 @@ echo "Fetching Github data"
 seven_days_ago=$(date -d "7 days ago" +%F)  
 
 # PRs Opened for more than 7 days
-github_data="\n<https://github.com/search?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+repo%3Aopenshift-pipelines%2Fconsole-plugin++-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&created:<${seven_days_ago} | PRs Opened for more than 7 days>:  $(curl -s "https://api.github.com/search/issues?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+repo%3Aopenshift-pipelines%2Fconsole-plugin++-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&created:<'${seven_days_ago}'" -H "Accept: application/json" | jq '.total_count')"
+github_data="\n<https://github.com/search?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+repo%3Aopenshift-pipelines%2Fconsole-plugin++-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&created:<${seven_days_ago} | PRs Opened for more than 7 days>:  $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+repo%3Aopenshift-pipelines%2Fconsole-plugin++-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&created:<'"${seven_days_ago}"' -H "Accept: application/json" | jq '.total_count')"'
+
+# PRs with No LGTM
 
 github_data+="\n<https://github.com/search?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+author%3Ayozaam+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&l=&l= | PRs with No LGTM>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aopenshift%2Fconsole+repo%3Aopenshift%2Fenhancements+repo%3Aopenshift%2Fconsole-operator+repo%3Aopenshift%2Fapi+-label%3Ado-not-merge%2Fwork-in-progress+-label%3Algtm+author%3Arohitkrai03+author%3Adebsmita1+author%3Ajeff-phillips-18+author%3AinvincibleJai+author%3Asahil143+author%3Avikram-raj+author%3Achristianvogt+author%3Ajerolimov+author%3AdivyanshiGupta+author%3Arottencandy+author%3Akarthikjeeyar+author%3Aabhinandan13jan+author%3Ayozaam+state%3Aopen+author%3ALucifergene+author%3Agruselhaus+author%3Alokanandaprabhu&type=pullrequests&ref=advsearch&l=&l=' -H "Accept: application/json" | jq '.total_count')"
 
