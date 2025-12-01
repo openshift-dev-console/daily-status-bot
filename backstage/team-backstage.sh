3e3e70b95843b730539c7604c8927c277c5b93b9
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2025-12-01 17:15:26 +0530

    Update team-backstage.sh

--- backstage/team-backstage.sh
+++ backstage/team-backstage.sh
@@ -1,5 +1,7 @@
 set -e
 
+authorization="Authorization: Bearer "$1
+
 echo "Generating Status Report"
 
 head="\nBackstage GitHub Status"
@@ -16,26 +18,26 @@ url5="https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-char
 url6="https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr+created%3A<$PREVIOUS_DT&type=Issues"
 url7="https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr+created%3A<$PREVIOUS_DT&type=Issues"
 
-github_data_a="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_a+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url1 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_a="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_a+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url1 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_c="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_c+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url2 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_c="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_c+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-operator+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url2 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_d="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_d+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url3 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_d="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_d+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-plugins+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url3 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_e="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_e+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url4 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_e="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_e+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Fred-hat-developers-documentation-rhdh+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url4 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_f="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_f+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url5 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_f="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_f+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-chart+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url5 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_g="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_g+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url6 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_g="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_g+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-local+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url6 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
-github_data_h="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr' -H "Accept: application/json" | jq '.total_count // 0')"
-github_data_h+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url7 -H "Accept: application/json" | jq '.total_count // 0')"
+github_data_h="\n<https://github.com/search?l=&q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr | Total open PRs>: $(curl -s 'https://api.github.com/search/issues?q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr' -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
+github_data_h+="\n<https://github.com/search?q=repo%3Aredhat-developer%2Frhdh-must-gather+state%3Aopen+type%3Apr+created%3A%3C$PREVIOUS_DT&type=Issues | PRs opened for more than a week>: $(curl -s $url7 -H "Accept: application/json" -H "$authorization" | jq '.total_count // 0')"
 
 echo "Posting on slack"
 
@@ -199,7 +201,19 @@ data='{
   ]
 }'
 
-curl -X POST -H "Content-type:application/json" --data "$data" $1
+# Display the data in a readable format
+# echo -e "\n=========================================="
+# echo -e "GitHub Status Report Preview"
+# echo -e "==========================================\n"
+# echo -e "rhdh:$github_data_a"
+# echo -e "\nrhdh-operator:$github_data_c"
+# echo -e "\nrhdh-plugins:$github_data_d"
+# echo -e "\nred-hat-developers-documentation-rhdh:$github_data_e"
+# echo -e "\nrhdh-chart:$github_data_f"
+# echo -e "\nrhdh-local:$github_data_g"
+# echo -e "\nrhdh-must-gather:$github_data_h"
+# echo -e "\n==========================================\n"
+
+curl -X POST -H "Content-type:application/json" --data "$data" $2
 
 echo -e "\nDone"
-
